5f9f3b9450fcd25bdb29aa7761a0aece290253cf
Turupawn <ahmed.hn.43@gmail.com>
2016-07-28 13:16:24 -0600

    map + canvas

--- data.js
+++ data.js
@@ -1,2 +1,6 @@
 //var data = [{a:10,b:20},{a:666,b:200},{a:666,b:200}];
-var data = [{name:'a',value:150},{name:'b',value:200},{name:'c',value:100}];
+var data = [
+{name:'a',value:150,text:"X",x:10,y:200},
+{name:'b',value:200,text:"Y",x:100,y:250},
+{name:'b',value:200},
+{name:'c',value:100,text:"X",x:150,y:500}];

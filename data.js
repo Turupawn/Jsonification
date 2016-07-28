09f87d1fd56edc49586ea3e6206d40a451b75ba3
Turupawn <ahmed.hn.43@gmail.com>
2016-07-28 14:12:13 -0600

    replacing js file for param

--- data.js
+++
@@ -1,9 +0,0 @@
-var data = [{a:10,b:20,lat: 8, lng: 8},{a:666,b:200},{a:666,b:200,lat: 10, lng: 9}];
-
-/*
-var data = [
-{name:'a',value:150,text:"X",x:10,y:200},
-{name:'b',value:200,text:"Y",x:100,y:250},
-{name:'b',value:200},
-{name:'c',value:100,text:"X",x:150,y:500}];
-*/

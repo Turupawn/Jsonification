cd2dcea3cb59a383c645765cd6e6262fb1dc557b
Turupawn <ahmed.hn.43@gmail.com>
2016-07-28 17:14:43 -0600

    test

--- data.js
+++ data.js
@@ -1,4 +1,16 @@
-var data = [{lat:10,lng:10},{lat:10,lng:15},{lat:8,lng:4}];
+var data = [
+{name:"Abs",color:"blue",value:10},
+{name:"Hips",color:"orange",value:11},
+{name:"Pla",color:"white",value:7},
+
+{text:"Makerbot",x:100,y:400},
+{text:"Prussa",x:500,y:300},
+{text:"Lulzbot",x:200,y:100},
+
+{lat:8,lng:4},
+{lat:11,lng:6},
+{lat:10,lng:7}
+];
 
 /*
 Special keys:

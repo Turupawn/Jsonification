04473b6c5bc11c9ba38e8fa2209993a65b4d3d3b
Ahmed Castro <ahmed.hn.43@gmail.com>
2016-07-28 18:41:00 -0600

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,11 @@ var data = [
 {name:"Abs",color:"blue",value:10},
 {name:"Hips",color:"orange",value:11},
 {name:"Pla",color:"white",value:7},
+
+{text:"Makerbot",x:100,y:400},
+{text:"Prussa",x:500,y:300},
+{text:"Lulzbot",x:200,y:100},
+
 ];
 
 /*
